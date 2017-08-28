e3e951995c1a69cfc67ee5ba1b5a5dac1eed614f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:50:59 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import attach from "./source"
+import attach from "./"
 
 test(({same, end}) => {
   same(
