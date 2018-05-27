f5ce8bb421979e60e574a9da5de48c3e5125b45f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-import {merge} from "most"
-import {of} from "most"
-import type from "@unction/type"
-
-const BEGINNING: Integer = 0
-
-export default function attach (key: KeyType | null): UnaryFunctionType {
-  return function attachKey (value: mixed): UnaryFunctionType {
-    return function attachKeyValue (functor: ObjectType | ArrayType | StringType | MapType | SetType): ObjectType | ArrayType | StringType | MapType | SetType {
+import { merge } from "most";
+import { of } from "most";
+import type from "@unction/type";
+const BEGINNING = 0;
+export default function attach(key) {
+  return function attachKey(value) {
+    return function attachKeyValue(functor) {
       switch (type(functor)) {
-        case "Object": {
-          return {
-            ...functor,
-            [key]: value,
+        case "Object":
+          {
+            return { ...functor,
+              [key]: value
+            };
           }
-        }
-        case "Array": {
-          return [
-            ...functor.slice(BEGINNING, key),
-            value,
-            ...functor.slice(key),
-          ]
-        }
-        case "String": {
-          return `${functor.slice(BEGINNING, key)}${value}${functor.slice(key)}`
-        }
-        case "Map": {
-          return new Map([...functor, [key, value]])
-        }
-        case "Set": {
-          return new Set([...functor, value])
-        }
-        case "Stream": {
-          return merge(functor, of(value))
-        }
-        default: {
-          throw new Error(`attach doesn't know how to set a key and value on ${type(functor)}`)
-        }
+
+        case "Array":
+          {
+            return [...functor.slice(BEGINNING, key), value, ...functor.slice(key)];
+          }
+
+        case "String":
+          {
+            return `${functor.slice(BEGINNING, key)}${value}${functor.slice(key)}`;
+          }
+
+        case "Map":
+          {
+            return new Map([...functor, [key, value]]);
+          }
+
+        case "Set":
+          {
+            return new Set([...functor, value]);
+          }
+
+        case "Stream":
+          {
+            return merge(functor, of(value));
+          }
+
+        default:
+          {
+            throw new Error(`attach doesn't know how to set a key and value on ${type(functor)}`);
+          }
       }
-    }
-  }
+    };
+  };
 }
