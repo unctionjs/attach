aeebd8390f4f5c3522d0b2880886459aed54f7d9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:42 -0700

    Autolinting

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-import { merge } from "most";
-import { of } from "most";
+import {merge} from "most";
+import {of} from "most";
 import type from "@unction/type";
 const BEGINNING = 0;
-export default function attach(key) {
-  return function attachKey(value) {
-    return function attachKeyValue(functor) {
+
+export default function attach (key) {
+  return function attachKey (value) {
+    return function attachKeyValue (functor) {
       switch (type(functor)) {
         case "Object":
-          {
-            return { ...functor,
-              [key]: value
-            };
-          }
+        {
+          return {...functor,
+            [key]: value,
+          };
+        }
 
         case "Array":
-          {
-            return [...functor.slice(BEGINNING, key), value, ...functor.slice(key)];
-          }
+        {
+          return [...functor.slice(BEGINNING, key), value, ...functor.slice(key)];
+        }
 
         case "String":
-          {
-            return `${functor.slice(BEGINNING, key)}${value}${functor.slice(key)}`;
-          }
+        {
+          return `${functor.slice(BEGINNING, key)}${value}${functor.slice(key)}`;
+        }
 
         case "Map":
-          {
-            return new Map([...functor, [key, value]]);
-          }
+        {
+          return new Map([...functor, [key, value]]);
+        }
 
         case "Set":
-          {
-            return new Set([...functor, value]);
-          }
+        {
+          return new Set([...functor, value]);
+        }
 
         case "Stream":
-          {
-            return merge(functor, of(value));
-          }
+        {
+          return merge(functor, of(value));
+        }
 
         default:
-          {
-            throw new Error(`attach doesn't know how to set a key and value on ${type(functor)}`);
-          }
+        {
+          throw new Error(`attach doesn't know how to set a key and value on ${type(functor)}`);
+        }
       }
     };
   };
--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-magic-numbers */
-import {test} from "tap"
-import {of} from "most"
-import streamSatisfies from "@unction/streamsatisfies"
+import {test} from "tap";
+import {of} from "most";
+import streamSatisfies from "@unction/streamsatisfies";
 
-import attach from "./index"
+import attach from "./index";
 
 test("Object (Empty)", ({same, end}) => {
   same(
     attach("hello")("world")({}),
     {hello: "world"}
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Object (Filled)", ({same, end}) => {
   same(
@@ -21,82 +21,82 @@ test("Object (Filled)", ({same, end}) => {
       hello: "world",
       test: "case",
     }
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Array (First)", ({same, end}) => {
   same(
     attach(0)("a")([]),
     ["a"]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Array (Too big index)", ({same, end}) => {
   same(
     attach(1)("a")([]),
     ["a"]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Array (Last)", ({same, end}) => {
   same(
     attach(1)("a")(["b"]),
     ["b", "a"]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Array (Middle)", ({same, end}) => {
   same(
     attach(1)("a")(["b", "c"]),
     ["b", "a", "c"]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("String (First)", ({same, end}) => {
   same(
     attach(0)("a")("bc"),
     "abc"
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("String (last)", ({same, end}) => {
   same(
     attach(1)("a")(""),
     "a"
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Map", ({same, end}) => {
   same(
     attach("aaa")("aaa")(new Map([["bbb", "bbb"], ["ccc", "ccc"]])),
     new Map([["aaa", "aaa"], ["bbb", "bbb"], ["ccc", "ccc"]])
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Set", ({same, end}) => {
   same(
     attach(1)("a")(new Set(["b"])),
     new Set(["b", "a"])
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test("Stream", ({equal, doesNotThrow, end}) => {
   streamSatisfies(
@@ -108,18 +108,18 @@ test("Stream", ({equal, doesNotThrow, end}) => {
   )(
     ({length}) =>
       (position) => {
-        equal(length, position)
-        end()
+        equal(length, position);
+        end();
       }
   )(
     attach(null)("b")(of("a"))
-  )
-})
+  );
+});
 
 test(({throws, end}) => {
   throws(
     () => attach({aaa: "aaa"})("aaa")(1)
-  )
+  );
 
-  end()
-})
+  end();
+});
